ac990b4ff953ffbc4a3bf27f3ccbcff76b891d04
DorceCode <dorceeditor@gmail.com>
2025-09-22 14:07:07 -0300

    Add files via upload

--- src/Validator.php
+++ src/Validator.php
@@ -8,19 +8,23 @@ class Validator
 
     public static function sanitizePassword(string $password): string
     {
-        return trim(filter_var($password, FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH));
+        return trim(filter_var(
+            $password,
+            FILTER_UNSAFE_RAW,
+            FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH
+        ));
     }
 
     public static function isValidEmail(string $email): bool
     {
         $email = self::sanitizeEmail($email);
+
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
 
     public static function isValidPassword(string $password): bool
     {
         $password = self::sanitizePassword($password);
-        return strlen($password) >= 8 && preg_match('/[A-Z]/', $password) && preg_match('/\d/', $password);
+        return (bool) preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/', $password);
     }
 }
-
